22d943e21a9f69719032ee9dedb541755ab22e16
Jian Yuan Lee <jianyuan@gmail.com>
2022-06-06 23:08:29 +0100

    Rewrite Client struct and functions

--- sentry/sentry.go
+++ sentry/sentry.go
@@ -1,6 +1,12 @@
 package sentry
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -8,15 +14,28 @@ import (
 	"github.com/dghubble/sling"
 )
 
-// public constants for external use
 const (
-	DefaultBaseURL = "https://sentry.io/api/"
-	APIVersion     = "0"
+	defaultBaseURL = "https://sentry.io/api/"
+	userAgent      = "go-sentry"
+
+	APIVersion = "0"
 )
 
-// Client for sentry api
+var errNonNilContext = errors.New("context must be non-nil")
+
+// Client for Sentry API.
 type Client struct {
-	sling               *sling.Sling
+	client *http.Client
+
+	// BaseURL for API requests.
+	BaseURL *url.URL
+
+	// User agent used when communicating with Sentry.
+	UserAgent string
+
+	// TODO: Remove sling
+	sling *sling.Sling
+
 	Organizations       *OrganizationService
 	OrganizationMembers *OrganizationMemberService
 	Teams               *TeamService
@@ -28,34 +47,27 @@ type Client struct {
 	Ownership           *ProjectOwnershipService
 }
 
+type service struct {
+	client *Client
+}
+
 // NewClient returns a new Sentry API client.
-// If a nil httpClient is given, the http.DefaultClient will be used.
-// If a nil baseURL is given, the DefaultBaseURL will be used.
-// If the baseURL does not have the suffix "/api/", it will be added automatically.
-func NewClient(httpClient *http.Client, baseURL *url.URL, token string) *Client {
+// If a nil httpClient is provided, the http.DefaultClient will be used.
+func NewClient(httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
 	}
+	baseURL, _ := url.Parse(defaultBaseURL)
 
-	if baseURL == nil {
-		baseURL, _ = url.Parse(DefaultBaseURL)
-	}
-	if !strings.HasSuffix(baseURL.Path, "/") {
-		baseURL.Path += "/"
-	}
-	if !strings.HasSuffix(baseURL.Path, "/api/") {
-		baseURL.Path += "api/"
-	}
-	baseURL.Path += APIVersion + "/"
-
-	base := sling.New().Base(baseURL.String()).Client(httpClient)
-
-	if token != "" {
-		base.Add("Authorization", "Bearer "+token)
-	}
+	// base := sling.New().Base(baseURL.String()).Client(httpClient)
+	base := sling.New().Client(httpClient)
 
 	c := &Client{
-		sling:               base,
+		sling: base,
+
+		client:  httpClient,
+		BaseURL: baseURL,
+
 		Organizations:       newOrganizationService(base.New()),
 		OrganizationMembers: newOrganizationMemberService(base.New()),
 		Teams:               newTeamService(base.New()),
@@ -67,6 +79,146 @@ func NewClient(httpClient *http.Client, baseURL *url.URL, token string) *Client 
 		Ownership:           newProjectOwnershipService(base.New()),
 	}
 	return c
+}
+
+// NewOnPremiseClient returns a new Sentry API client with the provided base URL.
+// Note that the base URL must be in the format "http(s)://[hostname]/api/".
+// If the base URL does not have the suffix "/api/", it will be added automatically.
+// If a nil httpClient is provided, the http.DefaultClient will be used.
+func NewOnPremiseClient(baseURL string, httpClient *http.Client) (*Client, error) {
+	baseEndpoint, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasSuffix(baseEndpoint.Path, "/") {
+		baseEndpoint.Path += "/"
+	}
+	if !strings.HasSuffix(baseEndpoint.Path, "/api/") {
+		baseEndpoint.Path += "api/"
+	}
+
+	c := NewClient(httpClient)
+	c.BaseURL = baseEndpoint
+	return c, nil
+}
+
+// NewRequest creates an API request.
+func (c *Client) NewRequest(method, urlRef string, body interface{}) (*http.Request, error) {
+	if !strings.HasSuffix(c.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
+	}
+
+	u, err := c.BaseURL.Parse(urlRef)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	return req, nil
+}
+
+// Response is a Sentry API response. This wraps the standard http.Response
+// and provides convenient access to things like pagination links and rate limits.
+type Response struct {
+	*http.Response
+
+	// TODO: Parse rate limit
+}
+
+func newResponse(r *http.Response) *Response {
+	response := &Response{Response: r}
+	return response
+}
+
+func (c *Client) BareDo(ctx context.Context, req *http.Request) (*Response, error) {
+	if ctx == nil {
+		return nil, errNonNilContext
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		// If we got an error, and the context has been canceled,
+		// the context's error is probably more useful.
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			return nil, err
+		}
+	}
+
+	response := newResponse(resp)
+
+	err = CheckResponse(resp)
+	return response, err
+}
+
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*Response, error) {
+	resp, err := c.BareDo(ctx, req)
+	if err != nil {
+		return resp, err
+	}
+	defer resp.Body.Close()
+
+	switch v := v.(type) {
+	case nil:
+	case io.Writer:
+		_, err = io.Copy(v, resp.Body)
+	default:
+		decErr := json.NewDecoder(resp.Body).Decode(v)
+		if decErr == io.EOF {
+			decErr = nil
+		}
+		if decErr != nil {
+			err = decErr
+		}
+	}
+	return resp, err
+}
+
+type ErrorResponse struct {
+	Response *http.Response
+	Detail   string `json:"detail"`
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf(
+		"%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Detail)
+}
+
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	// TODO: Handle API errors
+
+	return errorResponse
 }
 
 // Avatar represents an avatar.
