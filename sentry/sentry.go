232cd66713bb7877ff2bba0b0e68086e82871939
Jian Yuan Lee <jianyuan@gmail.com>
2022-06-08 22:34:14 +0100

    Add pointer to value converters

--- sentry/sentry.go
+++ sentry/sentry.go
@@ -415,11 +415,47 @@ type Rate struct {
 // Bool returns a pointer to the bool value passed in.
 func Bool(v bool) *bool { return &v }
 
+// BoolValue returns the value of the bool pointer passed in or
+// false if the pointer is nil.
+func BoolValue(v *bool) bool {
+	if v != nil {
+		return *v
+	}
+	return false
+}
+
 // Int returns a pointer to the int value passed in.
 func Int(v int) *int { return &v }
+
+// IntValue returns the value of the int pointer passed in or
+// 0 if the pointer is nil.
+func IntValue(v *int) int {
+	if v != nil {
+		return *v
+	}
+	return 0
+}
 
 // String returns a pointer to the string value passed in.
 func String(v string) *string { return &v }
 
+// StringValue returns the value of the string pointer passed in or
+// "" if the pointer is nil.
+func StringValue(v *string) string {
+	if v != nil {
+		return *v
+	}
+	return ""
+}
+
 // Time returns a pointer to the time.Time value passed in.
 func Time(v time.Time) *time.Time { return &v }
+
+// TimeValue returns the value of the time.Time pointer passed in or
+// time.Time{} if the pointer is nil.
+func TimeValue(v *time.Time) time.Time {
+	if v != nil {
+		return *v
+	}
+	return time.Time{}
+}
